2584eb1ff151995b6c2f148cfe3e9949e38549c3
prfrl <mmadsen1978@gmail.com>
2022-04-30 19:16:23 -0400

    2022-04-30 19:16:23

--- sync_git.sh
+++ sync_git.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-dt=$(date +"%Y-%m-%d %T")
+## Update the URL of origin remote using SSH
+# git remote set-url origin git@github.com:username/repo.git
+# git remote set-url origin git@github.com:prfrl/mlops_scaffold.git
+# Test URL remote
+# git remote show origin
+
+dt=$(date "+ %Y-%m-%d %T")
 
 git add .
 
